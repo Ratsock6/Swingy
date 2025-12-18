90bd1a436fcd4bd47bcd05d32ecced40eafa5c38
Ratsock <ratsockfofox@gmail.com>
2025-12-18 15:47:15 +0100

    Delete useless line

--- src/main/java/fr/aallouv/manager/GameManager.java
+++ src/main/java/fr/aallouv/manager/GameManager.java
@@ -6,20 +6,9 @@ import java.time.LocalDateTime;
 public class GameManager {
 
     private GameViews gameViews;
-    private final String logsFile;
 
     public GameManager(GameViews gameViews) {
         this.gameViews = gameViews;
-        logsFile = LocalDateTime.now().toString();
-    }
-
-    public void newLogs(String logs) {
-        String logsLine = LocalDateTime.now().toString() + " [LOGS]: " + logs;
-
-    }
-
-    public String getLogsFile() {
-        return logsFile;
     }
 
     public GameViews getGameViews() {
