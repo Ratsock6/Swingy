b2f037a8df13dc1729e0b60c8a3b03f7ffc24a41
Ratsock <ratsockfofox@gmail.com>
2025-12-29 19:26:50 +0100

    Add visited and monster defeated flags to SlotMap

--- src/main/java/fr/aallouv/manager/map/SlotMap.java
+++ src/main/java/fr/aallouv/manager/map/SlotMap.java
@@ -8,12 +8,20 @@ public class SlotMap {
     private EMapRoom eMapRoom;
     private final int id = idCounter++;
     private final int coordX, coordY;
+    private boolean visited, monsterDefeated;
 
     public SlotMap(EMapRoom mapRoom, int x, int y) {
 
         this.eMapRoom = mapRoom;
         this.coordX = x;
         this.coordY = y;
+        this.visited = false;
+        if (eMapRoom == EMapRoom.START)
+            this.visited = true;
+        if (eMapRoom == EMapRoom.COMBAT || eMapRoom == EMapRoom.ELITE || eMapRoom == EMapRoom.BOSS)
+            this.monsterDefeated = false;
+        else
+            this.monsterDefeated = true;
         App.getApp().getGameManager().getMapManager().getMaps().add(this);
         App.getApp().getLogger().log("Created SlotMap ID[" + this.id + "] of type " + this.eMapRoom.name() + " at (" + this.coordX + "," + this.coordY + ")");
     }
@@ -32,6 +40,22 @@ public class SlotMap {
 
     public int getCoordY() {
         return coordY;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public boolean isMonsterDefeated() {
+        return monsterDefeated;
+    }
+
+    public void setMonsterDefeated(boolean monsterDefeated) {
+        this.monsterDefeated = monsterDefeated;
     }
 }
 
