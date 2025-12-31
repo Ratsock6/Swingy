8ec43c0f078aa36ed76c068ee78e3b92c7c9485c
Ratsock <ratsockfofox@gmail.com>
2025-12-31 16:48:30 +0100

    Add x and y coordinates to Hero class; enhance constructor and add getter/setter methods

--- src/main/java/fr/aallouv/manager/entity/Hero.java
+++ src/main/java/fr/aallouv/manager/entity/Hero.java
@@ -5,6 +5,7 @@ import fr.aallouv.App;
 public class Hero extends Entity {
 
     private EClass eClass;
+    private int x, y;
 
     public Hero(String name, EClass eClass) {
         super(name, 1, eClass.getHitPoint(), eClass.getPhysicalAttack(), eClass.getPhysicalDefense(), eClass.getPsychicAttack(), eClass.getPsychicDefense(), eClass.getSpeed());
@@ -12,10 +13,26 @@ public class Hero extends Entity {
         App.getApp().getLogger().log("Created Hero: " + name + " of class " + eClass.getClassName());
     }
 
-    public Hero(String name, EClass eClass, int level, int hitPoints, int attack, int defense, int psychicAttack, int psychicDefense) {
-        super(name, hitPoints ,level, hitPoints, attack, defense, psychicAttack, psychicDefense);
+    public Hero(String name, EClass eClass, int level, int hitPoints, int attack, int defense, int psychicAttack, int psychicDefense, int speed) {
+        super(name ,level, hitPoints, attack, defense, psychicAttack, psychicDefense, speed);
         this.eClass = eClass;
         App.getApp().getLogger().log("Created Hero: " + name + " of class " + eClass.getClassName() + " at level " + level);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 
     public EClass geteClass() {
