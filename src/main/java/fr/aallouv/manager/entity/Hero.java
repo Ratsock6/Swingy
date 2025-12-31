43898086f8ef70a4629c66545874049cc31bf1f9
Ratsock <ratsockfofox@gmail.com>
2025-12-31 16:50:36 +0100

    Enhance onDeath method in Hero class; add logging for death and respawn events, reset position, and restore health

--- src/main/java/fr/aallouv/manager/entity/Hero.java
+++ src/main/java/fr/aallouv/manager/entity/Hero.java
@@ -45,5 +45,11 @@ public class Hero extends Entity {
 
     @Override
     public void onDeath() {
+        App.getApp().getLogger().log("Hero " + this.name + " has died.");
+        setX(0);
+        setY(0);
+        this.hitPoints = eClass.getHitPoint();
+        App.getApp().getLogger().log("Hero " + this.name + " has been respawned at starting position with full health.");
+        App.getApp().getGameManager().enterRoom(App.getApp().getGameManager().getMapManager().getSlotMapByCoordinates(x, y));
     }
 }
