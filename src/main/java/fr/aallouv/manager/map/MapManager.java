a38b1cb05b6bd9e3927c1bb59ebc8c38f25f685c
Ratsock <ratsockfofox@gmail.com>
2026-01-02 19:06:19 +0100

    Enhance MapManager with new methods for cardinal point movement and room visualization

--- src/main/java/fr/aallouv/manager/map/MapManager.java
+++ src/main/java/fr/aallouv/manager/map/MapManager.java
@@ -4,8 +4,10 @@ import fr.aallouv.App;
 import fr.aallouv.utils.Logger;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
+import java.util.stream.Stream;
 
 public class MapManager {
 
@@ -34,9 +36,10 @@ public class MapManager {
 			App.getApp().getLogger().log("Available cardinal points to add new room: " + cardinalPointsAvailable.toString());
 			CardinalPoint selectedCardinalPoint = cardinalPointsAvailable.get(new Random().nextInt(cardinalPointsAvailable.size()));
 			App.getApp().getLogger().log("Selected cardinal point: " + selectedCardinalPoint.name());
-			EMapRoom[] eMapRoomArrayList = EMapRoom.values();
-			EMapRoom selectedRoom = eMapRoomArrayList[new Random().nextInt(eMapRoomArrayList.length - 2)];
-
+			EMapRoom[] eMapRoomArrayList = Stream.of(EMapRoom.values())
+					.filter(room -> !room.isUnique())
+					.toArray(EMapRoom[]::new);
+			EMapRoom selectedRoom = eMapRoomArrayList[new Random().nextInt(eMapRoomArrayList.length)];
 			int finalX = slotMap.getCoordX() + selectedCardinalPoint.getAddX();
 			int finalY = slotMap.getCoordY() + selectedCardinalPoint.getAddY();
 			new SlotMap(selectedRoom, finalX, finalY);
@@ -96,6 +99,19 @@ public class MapManager {
 				cardinalPoints.remove(CardinalPoint.EAST);
 			if (slotsMap.getCoordX() == coordX - 1 && slotsMap.getCoordY() == coordY)
 				cardinalPoints.remove(CardinalPoint.WEST);
+		}
+		return cardinalPoints;
+	}
+
+	public ArrayList<CardinalPoint> getCardinalPointsHeroCanMove(int coordX, int coordY) {
+		ArrayList<CardinalPoint> cardinalPoints = new ArrayList<>();
+		cardinalPoints.add(CardinalPoint.NORTH);
+		cardinalPoints.add(CardinalPoint.SOUTH);
+		cardinalPoints.add(CardinalPoint.EAST);
+		cardinalPoints.add(CardinalPoint.WEST);
+		ArrayList<CardinalPoint> cardinalPointsEmpty = getCardinalPointsAvailable(coordX, coordY);
+		for (CardinalPoint cardinalPoint : cardinalPointsEmpty) {
+			cardinalPoints.remove(cardinalPoint);
 		}
 		return cardinalPoints;
 	}
@@ -166,24 +182,50 @@ public class MapManager {
 			}
 			App.getApp().getLogger().log(line.toString());
 		}
+	}
 
-//		for (int y = ordonneMax; y >= ordonneMin; y--) {
-//			StringBuilder line = new StringBuilder();
-//			for (int x = absisMin; x <= abscisseMax; x++) {
-//				boolean found = false;
-//				for (SlotMap slotMap : getMaps()) {
-//					if (slotMap.getCoordX() == x && slotMap.getCoordY() == y) {
-//						line.append("[").append(slotMap.geteMapRoom().getSymbol()).append("]");
-//						found = true;
-//						break;
-//					}
-//				}
-//				if (!found) {
-//					line.append("[ ]");
-//				}
-//			}
-//			System.out.println(line);
-//		}
+	public void viewVisitedRoom() {
+		System.out.println("Map View (Visited Rooms):");
+		int absisMin = 0, abscisseMax = 0, ordonneMin = 0, ordonneMax = 0;
+		for (SlotMap slotMap : getMaps()) {
+			if (slotMap.getCoordX() < absisMin)
+				absisMin = slotMap.getCoordX();
+			if (slotMap.getCoordX() > abscisseMax)
+				abscisseMax = slotMap.getCoordX();
+			if (slotMap.getCoordY() < ordonneMin)
+				ordonneMin = slotMap.getCoordY();
+			if (slotMap.getCoordY() > ordonneMax)
+				ordonneMax = slotMap.getCoordY();
+		}
+
+		int width = abscisseMax - absisMin + 1;
+		int height = ordonneMax - ordonneMin + 1;
+
+		int max = Math.max(width, height);
+
+		for (int y = 0; y < max; y++) {
+			StringBuilder line = new StringBuilder();
+			for (int x = 0; x < max; x++) {
+				boolean found = false;
+				for (SlotMap slotMap : getMaps()) {
+					if (slotMap.getCoordX() == x + absisMin && slotMap.getCoordY() == (ordonneMax - y)) {
+						if (!slotMap.isVisited()) {
+							line.append("[ ]");
+							found = true;
+							break;
+						}
+						line.append("[").append(slotMap.geteMapRoom().getSymbol()).append("]");
+						found = true;
+						break;
+					}
+				}
+				if (!found) {
+					line.append("[ ]");
+				}
+			}
+			App.getApp().getGameManager().printMessage(line.toString());
+		}
+
 	}
 
 	public SlotMap getSlotMapByCoordinates(int x, int y) {
