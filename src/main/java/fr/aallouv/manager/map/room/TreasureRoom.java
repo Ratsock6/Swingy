172b4e70713b60cf0469b9116996673ac27dd4e8
Ratsock <ratsockfofox@gmail.com>
2026-01-05 19:04:49 +0100

    Implement treasure collection logic in TreasureRoom on entry with user feedback

--- src/main/java/fr/aallouv/manager/map/room/TreasureRoom.java
+++ src/main/java/fr/aallouv/manager/map/room/TreasureRoom.java
@@ -1,6 +1,9 @@
 package fr.aallouv.manager.map.room;
 
+import fr.aallouv.App;
 import fr.aallouv.manager.map.EMapRoom;
+
+import java.util.Random;
 
 public class TreasureRoom extends Room {
 
@@ -10,6 +13,12 @@ public class TreasureRoom extends Room {
 
     @Override
     public void onEnter() {
+        if (!visited) {
+            App.getApp().getGameManager().getHero().setGold(App.getApp().getGameManager().getHero().getGold() + new Random().nextInt(100) + 50);
+            App.getApp().getGameManager().printMessage("You found a treasure room! You gained some gold. Current gold: " + App.getApp().getGameManager().getHero().getGold());
+        } else {
+            App.getApp().getGameManager().printMessage("You have already searched the chest in this room.");
+        }
     }
 
 }
