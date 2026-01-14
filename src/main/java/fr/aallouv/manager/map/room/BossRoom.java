e3a0f2498708af9fa60a7b98d06f061d5e2b02c4
Ratsock <ratsockfofox@gmail.com>
2026-01-14 15:20:14 +0100

    Implement combat logic for BossRoom, CombatRoom, and EliteRoom with victory, defeat, and flee scenarios

--- src/main/java/fr/aallouv/manager/map/room/BossRoom.java
+++ src/main/java/fr/aallouv/manager/map/room/BossRoom.java
@@ -1,5 +1,8 @@
 package fr.aallouv.manager.map.room;
 
+import fr.aallouv.App;
+import fr.aallouv.manager.ECombatStatus;
+import fr.aallouv.manager.combat.CombatManager;
 import fr.aallouv.manager.entity.Monster;
 import fr.aallouv.manager.map.EMapRoom;
 
@@ -14,6 +17,21 @@ public class BossRoom extends Room {
 
     @Override
     public void onEnter() {
+        if (!boss.isAlive()) {
+            System.out.println("The boss has already been defeated. Now you can find the exit!");
+        } else {
+            CombatManager combatManager = new CombatManager(boss);
+            ECombatStatus combatStatus = combatManager.startCombat();
+            if (combatStatus == ECombatStatus.VICTORY) {
+                System.out.println("Congratulations! You have defeated the boss!");
+            } else if (combatStatus == ECombatStatus.DEFEAT) {
+                System.out.println("You have been defeated by the boss. Try again!");
+            } else {
+                System.out.println("You fled from the boss fight. Prepare yourself for another encounter!");
+                App.getApp().getGameManager().getHero().setX(App.getApp().getGameManager().getHero().getLastX());
+                App.getApp().getGameManager().getHero().setY(App.getApp().getGameManager().getHero().getLastY());
+            }
+        }
     }
 
 }
--- src/main/java/fr/aallouv/manager/map/room/CombatRoom.java
+++ src/main/java/fr/aallouv/manager/map/room/CombatRoom.java
@@ -1,5 +1,8 @@
 package fr.aallouv.manager.map.room;
 
+import fr.aallouv.App;
+import fr.aallouv.manager.ECombatStatus;
+import fr.aallouv.manager.combat.CombatManager;
 import fr.aallouv.manager.entity.Monster;
 import fr.aallouv.manager.map.EMapRoom;
 
@@ -14,6 +17,21 @@ public class CombatRoom extends Room {
 
     @Override
     public void onEnter() {
+        if (!monster.isAlive()) {
+            System.out.println("The boss has already been defeated. Now you can find the exit!");
+        } else {
+            CombatManager combatManager = new CombatManager(monster);
+            ECombatStatus combatStatus = combatManager.startCombat();
+            if (combatStatus == ECombatStatus.VICTORY) {
+                System.out.println("Congratulations! You have defeated the boss!");
+            } else if (combatStatus == ECombatStatus.DEFEAT) {
+                System.out.println("You have been defeated by the boss. Try again!");
+            } else {
+                System.out.println("You fled from the boss fight. Prepare yourself for another encounter!");
+                App.getApp().getGameManager().getHero().setX(App.getApp().getGameManager().getHero().getLastX());
+                App.getApp().getGameManager().getHero().setY(App.getApp().getGameManager().getHero().getLastY());
+            }
+        }
     }
 
 }
